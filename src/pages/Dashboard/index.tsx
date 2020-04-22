7ecd772444f18c70a828652f0238e2fd261bda36
Patricio Martins <eupatriciomartins@gmail.com>
2020-04-22 05:25:08 -0300

    fix: init project

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import logoImg from '../../assets/github-logo.svg'
+import { Title } from './styles'
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+    </>
+  )
+}
+
+export default Dashboard
